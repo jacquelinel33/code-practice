6d1c0516b76613abb5fa81fa437a342c44a62e73
Jacqueline Lee <jlee4332@gmail.com>
2023-07-19 16:05:17 -0700

    add solution

--- categorize-new-member.js
+++ categorize-new-member.js
@@ -17,5 +17,4 @@ function openOrSenior(data) {
    return data.map(e => e[0] > 55 && e[1] > 7 ? "Senior" : "Open") 
 }
 
-
 console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]));
