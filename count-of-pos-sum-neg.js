445ae14e475df5709ce13c2ce11ad65fe6423af1
Jacqueline Lee <jlee4332@gmail.com>
2023-07-21 13:56:02 -0700

    add test

--- count-of-pos-sum-neg.js
+++ count-of-pos-sum-neg.js
@@ -11,7 +11,11 @@
 function countPositivesSumNegatives(input) {
   let positive = 0;
   let negative = 0;
-  if (input === null || input.length === 0 || (input.length === 2 && input[0] === 0 && input[1] === 0)) {
+  if (
+    input === null ||
+    input.length === 0 ||
+    (input.length === 2 && input[0] === 0 && input[1] === 0)
+  ) {
     return [];
   } else {
     input.forEach((c) => (c > 0 ? positive++ : (negative += c)));
@@ -24,3 +28,5 @@ console.log(
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
   ])
 );
+
+console.log(countPositivesSumNegatives([0, 0]));
