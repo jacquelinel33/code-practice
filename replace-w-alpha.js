0d8bdb9f419d0a5b5c63835391e3aadd91bf7d5f
Jacqueline Lee <jlee4332@gmail.com>
2023-07-28 13:37:50 -0700

    Add solution

--- replace-w-alpha.js
+++ replace-w-alpha.js
@@ -10,3 +10,18 @@
 // Example
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  let lowerCase = text.toLowerCase().split("");
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+  let newArray = lowerCase.map((e) => {
+    for (let x in letters) {
+      if (letters[x] === e) {
+        return Number(x) + 1;
+      }
+    }
+  });
+  return newArray.filter((num) => num !== undefined).join(" ");
+}
+
+console.log(alphabetPosition("Hello jelLo"));
