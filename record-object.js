875c9b15c91bc84b32d848bc37f1c16d2587fb33
Jacqueline Lee <jlee4332@gmail.com>
2023-08-02 11:15:05 -0700

    add solution

--- record-object.js
+++ record-object.js
@@ -21,16 +21,13 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
-  if (value.length === 0) {
-    delete records.prop;
-  } else if (prop !== "tracks" && value.length !== 0) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "" ) {
     records[id][prop] = value;
-  } else if (prop === 'tracks' && value.lenth !== 0) {
-    if (records[id][prop]) {
-      records[id][tracks] = value;
-    }
-    records[id].tracks = [];
-    records[id][prop] = value;
+  } else if (prop === 'tracks' && value !== "") {
+    records[id].hasOwnProperty(prop) ? records[id][prop].push(value) : records[id][prop] = [value]
+
   }
   return records;
 }
